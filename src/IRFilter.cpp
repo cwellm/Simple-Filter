e10ec0a5c12a48c2423d19b7de8f6614e602f22e
Christoph Wellm <christoph.wellm@creaflect.de>
2023-08-17 16:52:15 +0200

    Add IRFilter (#1)

---
+++ src/IRFilter.cpp
@@ -0,0 +1,17 @@
+#include <JuceHeader.h>
+
+
+namespace cw::Filter {
+
+	void test() {
+		juce::File file{ "C:\\Users\\christoph\\Playgrounds\\JUCE\\GUITEsts\\NewProject\\Resources\\background.png" };
+		juce::MemoryBlock mmBlock{};
+		if (!file.loadFileAsData(mmBlock)) {
+			throw 1;
+		}
+
+		juce::ImageFileFormat::loadFrom(juce::MemoryInputStream{ std::move(mmBlock) });
+	}
+
+
+} // cw::Filter

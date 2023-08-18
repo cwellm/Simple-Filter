6add3cc4e6304387293ca02139be8e58db661e63
Christoph Wellm <christoph.wellm@creaflect.de>
2023-08-18 16:21:05 +0200

    Extend CI
    - shift to Windows native system (dockerize later)
    - add native macos build (dockerize later)

--- CI/build.bat
+++ CI/build.bat
@@ -1,4 +1,13 @@
-set cmake="C:\Program Files\CMake\bin\cmake.exe"
-%cmake% --version
-%cmake% -S . -B build -DPROJECT_VERSION=1.0
-%cmake% --build build
+:: ----- use only with Docker -----
+::set cmake="C:\Program Files\CMake\bin\cmake.exe"
+::%cmake% --version
+::%cmake% -S . -B build -DPROJECT_VERSION=1.0
+::%cmake% --build build
+
+::if %errorlevel% 1 (
+::    exit 42
+::)
+
+cmake --version
+cmake -S . -B build -DPROJECT_VERSION=1.0
+cmake --build build
--- CI/build.sh
+++ CI/build.sh
@@ -1,3 +1,4 @@
+#!/bin/bash
 cmake -S . -B build -DPROJECT_VERSION=1.0
 cmake --build build
 
